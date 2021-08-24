0ea4d1e2dd63519721b81d6c4e757493af01cec9
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-24 15:34:19 +0800

    server-u1

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = requie('dotenv')
+
+dotenv.config()
+
+mongoose.connect()
 
 app.listen(4000, () => console.log("Server is up and running"))
