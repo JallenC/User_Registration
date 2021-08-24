4deb43bb67cbebca2a4c1c7c892a31a66735e4d3
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-24 14:58:37 +0800

    route-u1

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const signupTemplateCopy = require('../models/signupmodels')
 
 router.post('/signup', (request, response) => {
-    response.send('send')
+    const signedupUser = new signupTemplateCopy({
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
+    })
+    signedupUser.save()
+    .then(data => {
+        response.json(data)
+    })
+    .catch(error => {
+        response.json(error)
+    })
 })
 
 module.exports = router
