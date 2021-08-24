a35f9a77c8fec1361c044784a4182cb2e7122825
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-24 15:56:14 +0800

    server-u2-typofix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-const dotenv = requie('dotenv')
+const dotenv = require('dotenv')
 
 dotenv.config()
 
