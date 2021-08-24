8002edf6d5c4658dfd5c353ff8cdcf7e2cf9a94e
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-24 18:31:42 +0800

    server-u4

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const routesUrls = require('./routes/route')
+const cors = require('cors')
 
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database connected"))
 
-
+app.use(express.json())
+app.use(cors())
 app.use('/app', routesUrls)
 app.listen(4000, () => console.log("Server is up and running"))
