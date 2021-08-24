0b6cfbd751f90cbc99f52de719aa521b14893482
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-24 20:18:45 +0800

    indexjs-start

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import React from 'react';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+/* import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -15,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ */
