53ccdc5ab14dadcd8d9bf33a6747c7438422aa7a
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-08-25 14:44:43 +0800

    route-u2

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const signupTemplateCopy = require('../models/signupmodels')
+const bcrypt = require('bcrypt')
 
-router.post('/signup', (request, response) => {
+router.post('/signup', async (request, response) => {
+
+    const saltPassword = await bcrypt.genSalt(10)
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
     const signedupUser = new signupTemplateCopy({
         firstName: request.body.firstName,
         lastName: request.body.lastName,
         username: request.body.username,
         email: request.body.email,
-        password: request.body.password
+        password: securePassword
     })
     signedupUser.save()
     .then(data => {
