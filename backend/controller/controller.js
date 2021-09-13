562918f9b48bc95852474cdb35def8652ae1e9e9
Jallen31-Paper <64849924+Jallen31-Paper@users.noreply.github.com>
2021-09-13 14:20:54 +0800

    Fix

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -27,10 +27,47 @@ exports.create = (request, response) => {
 
 //find registered user
 exports.find= (request, response) => {
-
+    if(request.query.id) {
+        const id = request.query.id;
+        signupTemplateCopy.findById(id)
+        .then(data => {
+            if(data) {
+                response.status(404).send({message: "User not found"})
+            } else {
+                response.send(data)
+            }
+        })
+        .catch(error => {
+            response.status(500).send({message: "Error finding the user"})
+        })
+    } else {
+        signupTemplateCopy.find()
+        .then(user => {
+            response.send(user)
+        })
+        .catch(error => {
+            response.status(500).send({message: error.message||"Error occured while finding the user"})
+        })
+    }
 }
 
 //Update registered user
 exports.update = (request, response) => {
-    
+    if(!request.body) {
+        return response
+        .status(400)
+        .send({mesage:"Fill all the required fields to update"})
+    }
+    const id = request.parameters.id;
+    signupTemplateCopy.findByIdAndUpdate(id, request.body, {useFindAndModify: false})
+    .then(data => {
+        if(data) {
+            response.status(404).send({message: "Cannot update user"})
+        } else {
+            response.send(data)
+        }
+    })
+    .catch(error => {
+        response.status(500).send({message: "Error updating user"})
+    })
 }
